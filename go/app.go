7111f05f08726fd36618eae87dd81d6e336a9144
Eric Gomes <ericdsdev@gmail.com>
2024-10-15 02:01:45 -0300

    App go Criado

---
+++ go/app.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Full Cycle Rocks!!")
+}
+
+/*func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Full Cycle Rocks!!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+    // Escuta na porta 8888
+    http.ListenAndServe("8888:8888", nil)
+}*/
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module app.com/app
+
+go 1.23.2
