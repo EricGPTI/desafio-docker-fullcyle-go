f05782cd11a68707ab641eac927c543122558255
Eric Gomes <ericdsdev@gmail.com>
2024-10-15 02:02:20 -0300

    Removido comentarios

--- go/app.go
+++ go/app.go
@@ -7,13 +7,3 @@ import (
 func main() {
 	fmt.Println("Full Cycle Rocks!!")
 }
-
-/*func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Full Cycle Rocks!!")
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-    // Escuta na porta 8888
-    http.ListenAndServe("8888:8888", nil)
-}*/
